1caa6c5ca9a2b0a458edc5a1aa48c57c86f2f93f
djohnson <djohnson@daniel.local>
2016-08-02 16:39:14 -0700

    work with night theme

--- config.js
+++ config.js
@@ -9,9 +9,48 @@ config = {
    domainTo: 'pulldasher.cominor.com',
 
    // css style rule for a url that is open in another tab
-   getStyleFromUrl: function(url) {
+   getStylesFromUrl: function(url) {
+      // truncate anything after /pull/*/ since any url is the same pull
+      url = url.replace(/(pull\/\d+)[\/#].*$/, '$1');
+
       // select all links pointing to that url and give them a background
-      return 'a[href="' + url.replace(/(pull\/\d+)[\/#].*$/, '$1')
-           + '"]{background-color:#FDF6E3}';
+      return [
+         '.css-day_theme a[href="' + url + '"]{background-color:#FDF6E3}',
+         '.css-night_theme a[href="' + url + '"]{background-color:#1C2937}'
+      ];
+   },
+
+   // called to initialize the page on the content script, in case anything is
+   // needed for getStylesFromUrl to work properly.
+   initialize: function() {
+      var lastClass = null;
+
+      // put a class on body every time the theme changes
+      var setThemeClass = function() {
+         if (lastClass) {
+            document.body.classList.remove(lastClass);
+         }
+
+         var themeClass = null;
+
+         // get it from the element clicked, or from a <link> element
+         if (this.getAttribute && this.getAttribute('data-css')) {
+            themeClass = 'css-' + this.getAttribute('data-css');
+         } else {
+            themeClass = document.querySelector('link.active-theme')
+                                 .getAttribute('href')
+                                 .replace(/^.*\/([^\/]+)\.css$/, 'css-$1');
+         }
+
+         lastClass = themeClass;
+         document.body.classList.add(themeClass);
+      };
+
+      document.querySelectorAll('[data-css]').forEach(function(element) {
+         element.addEventListener('click', setThemeClass);
+      })
+
+      // set initial theme
+      setThemeClass();
    }
 };
--- content.js
+++ content.js
@@ -1,5 +1,7 @@
 (function() {
 
+config.initialize();
+
 // create a <style> tag that we can add rules to. this is useful because the
 // these styles will always apply, even if the page manipulates its dom.
 var styleElement = document.createElement('style');
@@ -17,11 +19,14 @@ function createRule(tab) {
       return;
    }
 
-   var style = config.getStyleFromUrl(tab.url);
+   var styles = config.getStylesFromUrl(tab.url);
 
    tabIdToUrl[tab.id] = tab.url;
-   styleSheet.insertRule(style, styleSheet.cssRules.length);
-   rules.push(style);
+
+   styles.forEach(function(style) {
+      styleSheet.insertRule(style, styleSheet.cssRules.length);
+      rules.push(style);
+   });
 }
 
 function removeRule(tab) {
@@ -31,16 +36,19 @@ function removeRule(tab) {
 
    // get its old url and use it to find the style rule
    var url = tabIdToUrl[tab.id];
-   var style = config.getStyleFromUrl(url);
-   var index = rules.indexOf(style);
+   var styles = config.getStylesFromUrl(url);
+   var index = rules.indexOf(styles[0]);
 
    if (index == -1) {
       return;
    }
 
    delete tabIdToUrl[tab.id];
-   styleSheet.deleteRule(index);
-   rules.splice(index, 1);
+
+   styles.forEach(function() {
+      styleSheet.deleteRule(index);
+      rules.splice(index, 1);
+   });
 
    // if multiple tabs create the same style rule, this only removes one of
    // them, so that all tabs have to be closed before the style is gone.
